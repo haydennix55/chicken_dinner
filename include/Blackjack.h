00c20f9af0e4c0de1a8ab4ff383354a1bc5a1711
hani2284 <hani2284@colorado.edu>
2018-02-28 17:02:37 -0700

    value and suit enums, card init, test init

--- include/Blackjack.h
+++ include/Blackjack.h
@@ -1,7 +1,20 @@
 #ifndef _BLACKJACK_H_
 #define _BLACKJACK_H_
 
+enum class Value { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, J, Q, K, A};
+enum class Suit { Hearts, Diamonds, Clubs, Spades};
 
+struct Card {
 
+    public:
+        Card(Suit suit, Value val) : suit_(suit), val_(val) {};
+        Value get_val() { return val_; }
+        Suit get_suit() { return suit_; }
+
+    private:
+        Value val_;
+        Suit suit_;
+
+};
 
 #endif
--- test/test.cpp
+++ test/test.cpp
@@ -2,3 +2,14 @@
 #include "catch.hpp"
 
 #include "Blackjack.h"
+
+TEST_CASE("Card struct","[card]"){
+    SECTION("can be created successfully and accessed"){
+        Card c(Suit::Spades, Value::A);
+        REQUIRE(c.get_val() == Value::A);
+        REQUIRE(c.get_suit() == Suit::Spades);
+
+    }
+
+
+}
