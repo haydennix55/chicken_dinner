d0aa8a5f143600106c2f63fc5914e180216b72d7
hani2284 <hayden.nix@colorado.edu>
2018-03-21 00:40:51 -0600

    implementation with todos for now, stopping point on gameplay methods and classes

--- src/Blackjack.cpp
+++ src/Blackjack.cpp
@@ -4,15 +4,16 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <tuple>
 
-bool operator==(Card lhs, Card rhs){
+bool operator==(Card lhs, Card rhs) {
     if (lhs.get_val() == rhs.get_val() && lhs.get_suit() == rhs.get_suit()){
         return true;
     }
     return false;
 }
 
-std::string SuitStringify(Suit suit){
+std::string SuitStringify(Suit suit) {
     if (suit == Suit::Spades){
         return "Spades";
     } else if (suit == Suit::Diamonds){
@@ -24,7 +25,7 @@ std::string SuitStringify(Suit suit){
     }
 }
 
-std::string ValueStringify(Value val){
+std::string ValueStringify(Value val) {
     switch (val) {
         case Value::Two : return "Two";
                         break;
@@ -70,7 +71,7 @@ CardFactory::CardFactory() {
     }
 }
 
-Deck::Deck(int number){
+Deck::Deck(int number) {
     //create a deck of n * 52 references to the cards
     for (int deck = 0; deck < number; deck++){
         for (int i = 0; i < 52; i++){
@@ -79,7 +80,7 @@ Deck::Deck(int number){
     }
 }
 
-Card* Deck::Draw(){
+Card* Deck::Draw() {
     Card *c = deck_[0];
     deck_.erase(deck_.begin());
     return c;
@@ -98,4 +99,107 @@ void Deck::Shuffle() {
     }
 
     deck_ = newDeck;
+}
+
+int Person::HandVal() {
+    std::vector<int> hand_values;
+    hand_values.push_back(0);
+    int card_val;
+
+    for (int i = 0; i < hand_.size(); i++) {
+
+        card_val = 0;
+        switch (hand_[i]->get_val()) {
+
+            case Value::Two : card_val = 2;
+                            break;
+            case Value::Three : card_val = 3;
+                            break;
+            case Value::Four : card_val = 4;
+                            break;
+            case Value::Five : card_val = 5;
+                            break;
+            case Value::Six : card_val = 6;
+                            break;
+            case Value::Seven : card_val = 7;
+                            break;
+            case Value::Eight : card_val = 8;
+                            break;
+            case Value::Nine : card_val = 9;
+                            break;
+            case Value::Ten : card_val = 10;
+                            break;
+            case Value::J : card_val = 10;
+                            break;
+            case Value::Q : card_val = 10;
+                            break;
+            case Value::K : card_val = 10;
+                            break;
+            case Value::A : card_val = 11;
+                            break;
+
+        }
+
+        int temp = hand_values.size();
+
+        if (card_val == 11){
+
+            for (int i = 0; i < temp; i++){
+                hand_values.push_back(hand_values[i]);
+            }
+            for (int i = temp; i < hand_values.size(); i++){
+                hand_values[i]++;
+            }
+        } else {
+
+            for (int i = 0; i < temp; i++) {
+                hand_values[i] += card_val;
+            }
+
+        }
+
+    }
+
+    int max = 0;
+
+    for (int i = 0; i < hand_values.size(); i++){
+        if (hand_values[i] > max && hand_values[i] <= 21){
+            max = hand_values[i];
+        }
+    }
+
+    return max;
+
+}
+
+void Game::deal_(Person *p) {
+    p->AddToHand(deck_.Draw());
+}
+
+void Game::burn_() {
+    discard_.AddToDeck(deck_.Draw());
+}
+
+void Game::SetupRound() {
+
+    //TODO: Collect bet
+
+    this->deal_(&player_);
+    this->deal_(&dealer_);
+
+    this->deal_(&player_);
+    this->deal_(&dealer_);
+}
+
+void Game::PlayRound() {
+    this->SetupRound();
+
+    //TODO: Check dealer insurance and Blackjack
+    //TODO: Check player for Blackjack
+
+    //TODO: Players turn until bust or Stand
+    //TODO: If player not busted, dealer turn until busted or > 17
+
+    //TODO: Assess results and payouts
+    //TODO: Clear table
 }
--- test/test.cpp
+++ test/test.cpp
@@ -73,7 +73,8 @@ TEST_CASE("Deck","[deck]"){
     }
     SECTION("draw function returns correct first card"){
         Card two_of_spades(Suit::Spades, Value::Two);
-        REQUIRE(*(deck.Draw()) == two_of_spades);
+        Card *c = deck.Draw();
+        REQUIRE(*(c) == two_of_spades);
     }
     SECTION("get_card returns correct cards"){
         Card two_of_spades(Suit::Spades,Value::Two);
@@ -111,4 +112,92 @@ TEST_CASE("shuffle method", "[shuffle]") {
 
     REQUIRE(deck.size() == oldDeck.size());
     REQUIRE(different);
+}
+
+TEST_CASE("Person's derived classes", "[person]") {
+    Person p;
+    Dealer d;
+
+    REQUIRE(p.get_hand_().size() == 0);
+    REQUIRE(d.get_hand_().size() == 0);
+
+    Card *c = new Card(Suit::Diamonds, Value::Q);
+    p.AddToHand(c);
+    d.AddToHand(c);
+
+    REQUIRE(p.get_hand_().size() == 1);
+    REQUIRE(d.get_hand_().size() == 1);
+
+    /* Must add more test cases when turns are implemented */
+
+}
+
+TEST_CASE("HandVal in Person base class", "[handval]") {
+    Person p;
+    Dealer d;
+
+    Card *two_of_hearts = new Card(Suit::Hearts, Value::Two);
+    Card *three_of_hearts = new Card(Suit::Hearts, Value::Three);
+
+    p.AddToHand(two_of_hearts);
+    d.AddToHand(two_of_hearts);
+
+    SECTION("determines value of single card hand (base case)") {
+        REQUIRE(p.HandVal() == 2);
+        REQUIRE(d.HandVal() == 2);
+    }
+
+    SECTION("determines value of two simple cards less than") {
+        p.AddToHand(three_of_hearts);
+        d.AddToHand(three_of_hearts);
+
+        REQUIRE(p.HandVal() == 5);
+        REQUIRE(d.HandVal() == 5);
+
+        Person p2;
+        Card *queen_of_diamonds = new Card(Suit::Diamonds, Value::Q);
+        p2.AddToHand(queen_of_diamonds);
+        p2.AddToHand(three_of_hearts);
+
+        REQUIRE(p2.HandVal() ==  13);
+
+
+    }
+
+    //TODO: Returns best value for two card hand with at lease one ace
+    //TODO: Returns value of n card hand with simple cards
+    //TODO: Returns value of n card hand with aces
+    //TODO: Returns 0 if no hand values are LEQ 21
+
+
+
+}
+
+TEST_CASE("Game class", "[game]") {
+    Game g2(2);
+    Game g6(6);
+
+    REQUIRE(g2.get_deck_().size() == 104);
+    REQUIRE(g6.get_deck_().size() == 312);
+
+    REQUIRE(g2.get_discard_().size() == 0);
+    REQUIRE(g6.get_discard_().size() == 0);
+
+    REQUIRE(g2.get_player_().get_hand_().size() == 0);
+    REQUIRE(g6.get_player_().get_hand_().size() == 0);
+    REQUIRE(g2.get_dealer_().get_hand_().size() == 0);
+    REQUIRE(g6.get_dealer_().get_hand_().size() == 0);
+
+    g2.PlayRound();
+    g6.PlayRound();
+
+    REQUIRE(g2.get_deck_().size() == 100);
+    REQUIRE(g6.get_deck_().size() == 308);
+
+    REQUIRE(g2.get_player_().get_hand_().size() == 2);
+    REQUIRE(g6.get_player_().get_hand_().size() == 2);
+    REQUIRE(g2.get_dealer_().get_hand_().size() == 2);
+    REQUIRE(g6.get_dealer_().get_hand_().size() == 2);
+
+
 }
